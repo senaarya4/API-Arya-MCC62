f3c05f9f2869c815142f143643cfff67e5c4c343
senaarya4 <senaarya4@gmail.com>
2022-01-31 13:16:10 +0700

    Aktivasi JWT

--- API/Controllers/AccountsController.cs
+++ API/Controllers/AccountsController.cs
@@ -1,14 +1,21 @@
 ﻿using API.Base;
+using API.Context;
 using API.Models;
 using API.Models.ViewModel;
 using API.Repository.Data;
 using API.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Net;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace API.Controllers
@@ -18,22 +25,49 @@ namespace API.Controllers
     public class AccountsController : BaseController<Account, AccountRepository, string>
     {
         private readonly AccountRepository accountRepository1;
-        public AccountsController(AccountRepository accountRepository) : base(accountRepository)
+        public IConfiguration _configuration;
+        public MyContext context;
+        public AccountsController(AccountRepository accountRepository, IConfiguration configuration, MyContext context) : base(accountRepository)
         {
             this.accountRepository1 = accountRepository;
+            this._configuration = configuration;
+            this.context = context;
         }
 
         [Route("login")]
         [HttpGet]
 
-        public ActionResult Login(RegisterVM registerVM)
+        public ActionResult<RegisterVM> Login(RegisterVM registerVM)
         {
             var result = accountRepository1.Login(registerVM);
             if (result > 0)
             {
                 if (result == 1)
                 {
-                    return StatusCode(200, new { status = HttpStatusCode.OK, result, message = "Berhasil Login!" });
+                    var getUserData = context.Employees.Where(e => e.Email == registerVM.Email || e.Phone == registerVM.Phone).FirstOrDefault();//email, role
+                    var account = context.Accounts.Where(a => a.NIK == getUserData.NIK).FirstOrDefault();
+                    var role = context.AccountRoles.Where(r => r.AccountId == account.NIK).FirstOrDefault();
+
+                    var claims = new List<Claim>
+                    {
+                        new Claim("Email", getUserData.Email),//payload
+                        new Claim("Roles", role.Role.RoleName)
+                    };
+
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:key"]));
+                    var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);//header
+                    var token = new JwtSecurityToken(
+                        _configuration["Jwt:Issuer"],
+                        _configuration["Jwt:Audience"],
+                        claims,
+                        expires: DateTime.UtcNow.AddMinutes(10),
+                        signingCredentials: signIn
+                        );
+
+                    var idtoken = new JwtSecurityTokenHandler().WriteToken(token);//gen token
+                    claims.Add(new Claim("TokenSecurity", idtoken.ToString()));
+                    
+                    return StatusCode(200, new { status = HttpStatusCode.OK, idtoken, message = "Berhasil Login!" });
                 }
                 else
                 {
@@ -44,6 +78,13 @@ namespace API.Controllers
             {
                 return StatusCode(404, new { status = HttpStatusCode.NotFound, result, message = "No Account Detected!" });
             }
+        }
+
+        [Authorize]
+        [HttpGet("TestJwt")]
+        public ActionResult TestJWT()
+        {
+            return Ok("Test JWT berhasil");
         }
 
         [Route("forget")]
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,5 +1,6 @@
 using API.Context;
 using API.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -9,9 +10,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace API
@@ -43,6 +46,26 @@ namespace API
             services.AddControllersWithViews()
                 .AddNewtonsoftJson(options =>
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +75,9 @@ namespace API
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseAuthentication();
+
+            app.UseAuthorization();
 
             app.UseHttpsRedirection();
 
